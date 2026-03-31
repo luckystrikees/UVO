879d12d393ea66fbd60bc0a67070ccf70c096290
luckystrikees <luckystrike.es@gmail.com>
2026-03-31 17:17:50 -0400

    up

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/', // <-- This tells Vite to look at the root of unitedvoices.online
   plugins: [
     react(),
     tailwindcss(),
